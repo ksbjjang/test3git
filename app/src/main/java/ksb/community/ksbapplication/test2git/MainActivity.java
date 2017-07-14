1abd7e586a49715ffac590515141e8c547e924d8
ksb <ksb5388@naver.com>
2017-07-14 09:23:22 +0900

    init2

--- app/src/main/java/ksb/community/ksbapplication/test2git/MainActivity.java
+++ app/src/main/java/ksb/community/ksbapplication/test2git/MainActivity.java
@@ -2,8 +2,11 @@ package ksb.community.ksbapplication.test2git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linear1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
